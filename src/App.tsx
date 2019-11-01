8208a70cce0a6a76d18e6657bf2004eb57ae0ea3
kubo-hide-kun <hide44041@gmail.com>
2019-11-01 13:22:13 +0900

    Remove waste import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React,{useState, useEffect, useCallback} from 'react';
-import * as BootStrap from 'react-bootstrap';
-import logo from './logo.svg';
 import './App.css';
 import Env from "./firebase"
 
